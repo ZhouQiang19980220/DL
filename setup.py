1b8ac5bde347d6a831dbaddfabab437920f4e379
ZhouQiang1998 <zhou_qiang98@163.com>
2024-08-28 19:31:01 +0800

    重新组织目录

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DL',
-    version='0.1.0',
+    name='zqdl',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'torch',
